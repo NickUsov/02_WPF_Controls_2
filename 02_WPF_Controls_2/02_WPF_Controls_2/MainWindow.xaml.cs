33cbb4343bf82ab86ec2ddd0d9e8b24cb0014370
NickUsov <nicky.yalta.ua@gmail.com>
2019-06-18 19:22:58 +0300

    base of calculator

---
+++ 02_WPF_Controls_2/02_WPF_Controls_2/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_WPF_Controls_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Bt1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtPnt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtEql_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtPls_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtMns_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtMlt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtDvd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtC_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtBS_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bt0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
